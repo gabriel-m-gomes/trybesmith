eb36b12330b18d8d09a8f4792c83190ee581984a
Gabriel Macedo Gomes <gabriel007.macedo@gmail.com>
2023-10-18 19:41:59 -0300

    requisito 5

--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import productController from '../controller/product.controller';
+import verifyBodyProduct from '../middlewares/verifyBodyProduct';
 
 const productRouter = Router();
 
-productRouter.post('/products', productController.inserProduct);
+productRouter.post(
+  '/products', 
+  verifyBodyProduct.verifyName, 
+  verifyBodyProduct.verifyPrice,
+  productController.inserProduct,
+);
 
 productRouter.get('/products', productController.AllProducts);
 
